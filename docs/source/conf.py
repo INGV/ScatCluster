8b07718a53edc58ea2a6c85a14248711a34c2b9c
zerafachris PERSONAL <zerafachris@gmail.com>
2024-04-28 22:12:47 +0200

    Refactor documentation config and workflows, standardize quotes, and remove redundant extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,14 +1,14 @@
 import os
 import sys
-import sphinx_material
 
+import sphinx_material
 from pkg_resources import DistributionNotFound, get_distribution
 
 # Fanciness to get version number
 try:
-    __version__ = get_distribution("scatcluster").version
+    __version__ = get_distribution('scatcluster').version
 except DistributionNotFound:
-    __version__ = "dev"
+    __version__ = 'dev'
 
 # Path setup
 sys.path.insert(0, os.path.abspath('../../'))
@@ -16,11 +16,10 @@ sys.path.insert(0, os.path.abspath('../../'))
 # Project information
 project = 'scatcluster'
 author = 'Christopher Zerafa and Carlo Giunchi'
-copyright = '2024, '+ author
+copyright = '2024, ' + author
 
 version = __version__
 release = __version__
-
 
 # General configuration
 exclude_patterns = [
@@ -31,27 +30,14 @@ exclude_patterns = [
 ]
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx_gallery.load_style',
-    'sphinx_favicon',
-    'sphinx_design',
-    'autoapi.extension',
-    'nbsphinx',
-    'numpydoc',
-    'IPython.sphinxext.ipython_console_highlighting',
-    'sphinx_copybutton',
-    "nbsphinx",
-    "rtds_action"
+    'sphinx.ext.duration', 'sphinx.ext.doctest', 'sphinx.ext.autodoc', 'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'sphinx_gallery.load_style',
+    'sphinx_favicon', 'sphinx_design', 'autoapi.extension', 'nbsphinx', 'numpydoc',
+    'IPython.sphinxext.ipython_console_highlighting', 'sphinx_copybutton', 'nbsphinx', 'rtds_action'
 ]
 
 favicons = [
-    "scatcluster_no_text.png",
+    'scatcluster_no_text.png',
 ]
 
 intersphinx_mapping = {
@@ -99,48 +85,46 @@ autosummary_generate = True
 # Theme options are theme-specific and customize the look and feel of a theme
 # further. For a list of options available for each theme, see the
 # documentation.
-html_logo = "_static/scatcluster_no_text.png"
+html_logo = '_static/scatcluster_no_text.png'
 
 # Language
 language = 'en'
 
 nbsphinx_allow_errors = True
 
-
 # The name of your GitHub repository
-rtds_action_github_token = os.environ["GITHUB_TOKEN"]
-rtds_action_github_repo = "INGV/ScatCluster"
+rtds_action_github_token = os.environ['GITHUB_TOKEN']
+rtds_action_github_repo = 'INGV/ScatCluster'
 # A GitHub personal access token is required, more info below
 # Whether or not to raise an error on Read the Docs if the
 # artifact containing the notebooks can't be downloaded (optional)
 rtds_action_error_if_missing = False
 
-
-extensions.append("sphinx_material")
+extensions.append('sphinx_material')
 html_theme_path = sphinx_material.html_theme_path()
 html_context = sphinx_material.get_html_context()
-html_theme = "sphinx_material"
-html_title = "ScatCluster"
-html_short_title = "ScatCluster"
+html_theme = 'sphinx_material'
+html_title = 'ScatCluster'
+html_short_title = 'ScatCluster'
 html_theme_options = {
-    "nav_title": "scatcluster",
-    "logo_icon": "&#xe869",
-    "color_primary": "blue",
-    "color_accent": "light-blue",
-    "repo_url": "https://github.com/INGV/ScatCluster",
-    "repo_name": "scatcluster",
-    "globaltoc_depth": 1,
-    "globaltoc_collapse": False,
-    "globaltoc_includehidden": False,
-    "nav_links": [],
+    'nav_title': 'scatcluster',
+    'logo_icon': '&#xe869',
+    'color_primary': 'blue',
+    'color_accent': 'light-blue',
+    'repo_url': 'https://github.com/INGV/ScatCluster',
+    'repo_name': 'scatcluster',
+    'globaltoc_depth': 1,
+    'globaltoc_collapse': False,
+    'globaltoc_includehidden': False,
+    'nav_links': [],
 }
 # HTML theme
 html_show_sourcelink = False
 html_sidebars = {
-    "**": [
-        "logo-text.html",
-        "globaltoc.html",
-        "localtoc.html",
-        "searchbox.html",
+    '**': [
+        'logo-text.html',
+        'globaltoc.html',
+        'localtoc.html',
+        'searchbox.html',
     ]
 }
